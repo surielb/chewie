9796cae2ba1c41e36906aebc391662184565a099
Suriel Bendahan <surielb@users.noreply.github.com>
2021-01-30 19:36:25 +0200

    Update chewie_player.dart

--- lib/src/chewie_player.dart
+++ lib/src/chewie_player.dart
@@ -83,7 +83,7 @@ class ChewieState extends State<Chewie> {
       Animation<double> animation,
       _ChewieControllerProvider controllerProvider) {
     return Scaffold(
-      resizeToAvoidBottomPadding: false,
+      resizeToAvoidBottomInset: false,
       body: Container(
         alignment: Alignment.center,
         color: Colors.black,
